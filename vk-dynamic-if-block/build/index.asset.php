a164bff75580a254bb2590e005cdcdc34c518bc5
Sean Murphy <sean@iamseanmurphy.com>
2025-09-18 07:18:12 +0000

    1.4.0

--- vk-dynamic-if-block/build/index.asset.php
+++ vk-dynamic-if-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '02fdf3dd668c87ec8624');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b53d74664a6519d8fd35');
--- vk-dynamic-if-block/vendor/composer/installed.php
+++ vk-dynamic-if-block/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.3.2',
-        'version' => '1.3.2.0',
-        'reference' => 'f3d4931defebd7337ddeaa53fa7aeaae06d4b201',
+        'pretty_version' => '1.4.0',
+        'version' => '1.4.0.0',
+        'reference' => '699a2fe7267ed07a4205672f7175a272b7923daf',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.3.2',
-            'version' => '1.3.2.0',
-            'reference' => 'f3d4931defebd7337ddeaa53fa7aeaae06d4b201',
+            'pretty_version' => '1.4.0',
+            'version' => '1.4.0.0',
+            'reference' => '699a2fe7267ed07a4205672f7175a272b7923daf',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
